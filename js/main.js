2c853c132e24a52e0500b877e233c4adfb09b942
Marco Rinaldi <marcorinaldi98@libero.it>
2023-02-22 14:50:24 +0100

    Javascript + html completati

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/* 
+JAVASCRIPT
+*/
+
+// chiedo all'utente quanti chilometri vuole percorrere
+const kmDaPercorrere = parseInt(prompt('Inserire il numero di chilometri che si vogliono percorrere:'));
+console.log(kmDaPercorrere);
+
+// chiedo all'utente l'età del passeggero
+const anniPasseggero = parseInt(prompt('Inserire gli anni di età del passeggero'));
+console.log(anniPasseggero);
+
+// definisco la tariffa per ogni chilometro
+const prezzoAlKm = 0.21;
+console.log(prezzoAlKm);
+
+// definisco il prezzo del biglietto odinario
+let tariffa = kmDaPercorrere * prezzoAlKm;
+console.log(tariffa);
+
+// definisco la variabile per l'eventuale sconto applicato 
+let prezzoBiglietto;
+
+// funzione per il calcolo dello sconto sul prezzo del biglietto in base all'età
+if (anniPasseggero < 18) {
+    prezzoBiglietto = (tariffa - ((tariffa * 20) / 100)).toFixed(2);
+    document.getElementById('tariffa').innerHTML = 'Hai diritto alla tariffa junior che prevede uno sconto del 20%';
+    document.getElementById('frase').innerHTML = 'Quindi il prezzo del tuo biglietto è di: ';
+    document.getElementById('prezzo').innerHTML = prezzoBiglietto + '€';
+    console.log(prezzoBiglietto);
+} else if (anniPasseggero > 65) {
+    prezzoBiglietto = (tariffa - ((tariffa * 40) / 100)).toFixed(2);
+    document.getElementById('tariffa').innerHTML = 'Hai diritto alla tariffa senior che prevede uno sconto del 40%';
+    document.getElementById('frase').innerHTML = 'Quindi il prezzo del tuo biglietto è di: ';
+    document.getElementById('prezzo').innerHTML = prezzoBiglietto + '€';
+    console.log(prezzoBiglietto);
+} else {
+    prezzoBiglietto = tariffa.toFixed(2);
+    document.getElementById('tariffa').innerHTML = 'Hai diritto alla tariffa base che non prevede sconti';
+    document.getElementById('frase').innerHTML = 'Quindi il prezzo del tuo biglietto è di: ';
+    document.getElementById('prezzo').innerHTML = prezzoBiglietto + '€';
+    console.log(prezzoBiglietto);
+}
+
